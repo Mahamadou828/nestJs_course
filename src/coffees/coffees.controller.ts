1e4d1363c30e841a97b24703ecd80c7e36eeae9f
Mahamadou Samaké <59400249+Mahamadou828@users.noreply.github.com>
2021-03-01 16:46:55 +0300

    courses(nestJs): chapter 2 finish

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,42 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+  @Get()
+  findAll(@Query() pagination) {
+    return this.coffeesService.findAll(pagination);
+  }
+
+  @Get(':id')
+  findCoffeeById(@Param('id') id: number) {
+    return this.coffeesService.findOne(id);
+  }
+
+  @Post()
+  createCoffee(@Body() body: CreateCoffeeDto) {
+    return this.coffeesService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: number, @Body() body: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, body);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
+  }
+}
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Coffee } from './entity/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll(pagination: any) {
+    return this.coffees;
+  }
+
+  findOne(id: number) {
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (coffee) throw new NotFoundException(`Coffee not found`);
+    return coffee;
+  }
+
+  create(createCoffeeDto: CreateCoffeeDto) {
+    this.coffees.push({
+      ...createCoffeeDto,
+      id: 5,
+    });
+  }
+
+  update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // update the existing entity
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
